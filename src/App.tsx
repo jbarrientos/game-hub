0d94a97024e6dcf2899876b6b2b261bcac894726
Jorge Barrientos <jorge.barrientos@gmail.com>
2023-05-12 14:43:02 -0700

    Build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from './components/NavBar'
 
 function App() {
   return <Grid templateAreas={{
     base: `"nav" "main"`,
     lg: `"nav nav" "aside main"` //1024 px
   }}>
-    <GridItem area="nav" bg="coral">Nav</GridItem>
+    <GridItem area="nav">
+      <NavBar/>
+    </GridItem>
     <Show above='lg'>
       <GridItem area="aside" bg="gold">Aside</GridItem>
     </Show>
